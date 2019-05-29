58ea01f1168c5fde28d630379d65fffa8f3c031b
dasunpubudumal <dasun.15@cse.mrt.ac.lk>
2019-05-29 17:13:33 +0530

    readme updated

--- speech_recognition_working.py
+++ speech_recognition_working.py
@@ -27,8 +27,8 @@ model_path = get_model_path()
 speech = LiveSpeech(
     sampling_rate=16000,
     hmm=os.path.join(model_path, 'en-us'),
-    lm='/home/akhitha/pocketsphinx/7723.lm.bin',
-    dic='/home/akhitha/pocketsphinx/7723.dic'
+    lm='/home/dasun/Documents/FYPpocketsphinx_python/7723.lm.bin',
+    dic='/home/dasun/Documents/FYPpocketsphinx_python/7723.dic'
 )
 
 for phrase in speech:
