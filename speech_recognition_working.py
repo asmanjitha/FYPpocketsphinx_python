1835ce7ed69e2c3f0c5540d8a9762a2f6dd13eb9
dasunpubudumal <dasun.15@cse.mrt.ac.lk>
2019-05-29 17:18:52 +0530

    code refactoring

--- speech_recognition_working.py
+++ speech_recognition_working.py
@@ -1,28 +1,7 @@
-# import os
-# from pocketsphinx import LiveSpeech, get_model_path
-
-# model_path = get_model_path()
-# print (model_path)
-
-# # from pocketsphinx import LiveSpeech
-
-# for phrase in LiveSpeech(): print(phrase)
-
 import os
 from pocketsphinx import LiveSpeech, get_model_path
 
 model_path = get_model_path()
-
-# speech = LiveSpeech(
-#     verbose=False,
-#     sampling_rate=16000,
-#     buffer_size=2048,
-#     no_search=False,
-#     full_utt=False,
-#     hmm=os.path.join(model_path, 'en-us'),
-#     lm=os.path.join('/home/pocketsphinx', '7723.lm'),
-#     dic=os.path.join('/home/pocketsphinx', '7723.dic')
-# )
 
 speech = LiveSpeech(
     sampling_rate=16000,
